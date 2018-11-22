ecb002b8d9e36928c80584808d4e5d596d4c59c1
vmkanczes <vkanczes@asu.edu>
2018-11-22 13:20:39 -0800

    Rebuilt room and verify and added hive...

--- src/main/TestDesignPatterns.java
+++ src/main/TestDesignPatterns.java
@@ -1,6 +1,10 @@
 package main;
 
 import builder.BroodRoomBuilderImpl;
+import builder.GermanHiveBuilder;
+import builder.Hive;
+import builder.HiveBuilder;
+import builder.HiveDirector;
 import builder.IRoomBuilder;
 import builder.RestRoomBuilderImpl;
 import builder.RoomBuildDirector;
@@ -9,28 +13,13 @@ public class TestDesignPatterns {
 
     public static void main(String[] args) {
 
-        // build hive
-        /*RoomBuilder broodRoom = new BroodRoomBuilder();
-        RoomDirector broodRoomDirector = new RoomDirector(broodRoom);
-        broodRoomDirector.makeRoom();
-        
-        Room broodRoom1 = broodRoomDirector.getRoom();
-        Room broodRoom2 = broodRoomDirector.getRoom();
-        
-        // build a rest/feed room
-        RoomBuilder restRoom = new RestRoomBuilder();
-        RoomDirector restRoomDirector = new RoomDirector(restRoom);
-        restRoomDirector.makeRoom();
-        
-        Room restRoom1 = restRoomDirector.getRoom();
-        Room restRoom2 = restRoomDirector.getRoom();
+        HiveBuilder germanHive = new GermanHiveBuilder();
+        HiveDirector hiveDirector = new HiveDirector(germanHive);
+        hiveDirector.makeHive();
 
-        
-        System.out.println("New Brood Room: " + broodRoom1.getType());
-        System.out.println("New Brood Room: " + broodRoom2.getType());
-        System.out.println("New Rest Room: " + restRoom1.getType());
-        System.out.println("New Rest Room: " + restRoom2.getType());
-*/
+        Hive newHive = hiveDirector.getHive();
+        System.out.println(newHive.getType());
+       
         // Build a brood room
         final IRoomBuilder broodBuilder = new BroodRoomBuilderImpl();
         final RoomBuildDirector BroodBuildDirector = new RoomBuildDirector(broodBuilder);
@@ -41,5 +30,7 @@ public class TestDesignPatterns {
         final RoomBuildDirector RestBuildDirector = new RoomBuildDirector(restBuilder);
         System.out.println(RestBuildDirector.construct());
     }
+    
+    
 
 }
