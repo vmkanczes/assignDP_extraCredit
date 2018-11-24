8097662730c95f4a7525c6fbaaf1ea6c613c5a03
vmkanczes <vkanczes@asu.edu>
2018-11-24 09:21:07 -0800

    Added junit tests to build and remove hives for the builder design pattern

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,23 @@
+/**
+File: AllTests.java
+Author: @author
+Date: Nov 24, 2018
+
+Description: 
+ */
+package test.java;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+Class:
+
+Description:
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ BeeAttributeTest.class })
+public class AllTests {
+
+}
