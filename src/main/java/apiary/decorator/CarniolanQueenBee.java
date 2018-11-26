e92b660d3b4a4ec88d296c1807ac7839974d0811
vmkanczes <vkanczes@asu.edu>
2018-11-25 18:24:07 -0800

    Added Queens

---
+++ src/main/java/apiary/decorator/CarniolanQueenBee.java
@@ -0,0 +1,131 @@
+package main.java.apiary.decorator;
+
+/**
+File: CarniolanWorkerBee.java
+Author: vkanczes
+Date: Nov 24, 2018
+
+Description: creates the bee attributes for species CarniolanWorkerBee.
+ */
+
+import main.java.apiary.builder.BeeType;
+
+/**
+ * Class: CarniolanWorkerBee
+ * 
+ * <p>Description: This class is to create an Carniolan bee with specific
+ * attributes.
+ */
+public class CarniolanQueenBee extends AttributeDecoratorImpl {
+
+    /**
+     * Constructor Input: newBee
+     * 
+     * <p>Description: constructor to a bee.
+     */
+    public CarniolanQueenBee(IBee newBee) {
+        super(newBee);
+
+    }
+
+    /**
+     * Method: getType 
+     * Inputs: 
+     * Returns: enum BeeType
+     * 
+     * <p>Description: Gets the type of bee.
+     */
+    @Override public BeeType getType() {
+        return BeeType.CARNIOLAN;
+    }
+
+    /**
+     * Method: getGentleness 
+     * Inputs: 
+     * Returns: int bee's level of gentleness
+     * 
+     * <p>Description: Get the bee's friendliness.
+     */
+    @Override public int getGentleness() {
+        return tempBee.getGentleness();
+    }
+
+    /**
+     * Method: getHoneyProduction 
+     * Inputs: 
+     * Returns: int bee's level of honey
+     * production
+     * 
+     * <p>Description: Get the bee's ability to produce honey.
+     */
+    @Override public int getHoneyProduction() {
+        return tempBee.getHoneyProduction() - 3;
+    }
+
+    /**
+     * Method: getPollination 
+     * Inputs: 
+     * Returns: int bee's ability to pollinate
+     * 
+     * <p>Description: Get the bee's ability to pollinate flowers.
+     */
+    @Override public int getPollination() {
+        return tempBee.getPollination() - 3;
+
+    }
+
+    /**
+     * Method: getReproduction 
+     * Inputs: 
+     * Returns: int bee's ability to reproduce
+     * 
+     * <p>Description: Get the bee's ability to help make baby bees.
+     */
+    @Override public int getReproduction() {
+        return tempBee.getReproduction() + 2;
+    }
+
+    /**
+     * Method: getWaxProduction 
+     * Inputs: 
+     * Returns: int bee's ability to create wax
+     * 
+     * <p>Description: Get the bee's ability to make wax to build the hive.
+     */
+    @Override public int getWaxProduction() {
+        return tempBee.getWaxProduction() - 3;
+    }
+
+    /**
+     * Method: getRole 
+     * Inputs: 
+     * Returns: int bee's role
+     * 
+     * <p>Description: Get the bee's role such as queen, worker, drone or spawner.
+     */
+    @Override public BeeRole getRole() {
+        return BeeRole.QUEEN;
+    }
+
+    /**
+     * Method: toString
+     * Inputs:
+     * Outputs: String of bee attribute information
+     * 
+     * <p>Description:  Creates string containing details about the bee.
+     */
+    public String toString() {
+        
+        String beeAttributes = (getType() + " "
+                + getRole() + " " 
+                + "Gentleness: " + getGentleness() + " "
+                + "Honey Production: " + getHoneyProduction() + " "
+                + "Pollination: " + getPollination() + " "
+                + "Reproduction: " + getReproduction() + " "
+                + "Wax Production: " + getWaxProduction() + " "
+                );
+        
+        return beeAttributes;
+    }
+
+}
---
+++ src/main/java/apiary/decorator/ItalianQueenBee.java
@@ -0,0 +1,124 @@
+package main.java.apiary.decorator;
+
+/**
+File: ItalianWorkerBee.java
+Author: vkanczes
+Date: Nov 24, 2018
+
+Description: creates the bee attributes for species ItalianWorkerBee.
+ */
+
+import main.java.apiary.builder.BeeType;
+
+/**
+Class: ItalianWorkerBee
+
+Description: This class is to create an German ordinary bee with specific attributes.
+ */
+public class ItalianQueenBee extends AttributeDecoratorImpl {
+    
+    public ItalianQueenBee(IBee newBee) {
+        super(newBee);
+        
+    }
+
+    /**
+     Method: getType
+     Inputs: 
+     Returns: enum BeeType
+     
+     Description:  Gets the type of bee.
+     */
+    @Override public BeeType getType() {
+        return BeeType.ITALIAN;
+    }
+    
+    /**
+     Method:  getGentleness
+     Inputs: 
+     Returns: int bee's level of gentleness
+     
+     Description: Get the bee's friendliness.
+     */
+    @Override public int getGentleness() {
+        return tempBee.getGentleness() - 1;
+    }
+    
+    /**
+     Method:  getHoneyProduction
+     Inputs: 
+     Returns: int bee's level of honey production
+     
+     Description:  Get the bee's ability to produce honey.
+     */
+    @Override public int getHoneyProduction() {
+        return tempBee.getHoneyProduction() - 3;
+    }
+    
+    /**
+     Method: getPollination
+     Inputs: 
+     Returns: 
+     
+     Description:  Get the bee's ability to pollinate flowers.
+     */
+    @Override public int getPollination() {
+        return tempBee.getPollination() - 3;
+
+    }
+    
+    /**
+     Method:  getReproduction
+     Inputs: 
+     Returns: int 
+     
+     Description:  Get the bee's ability to help make baby bees.
+     */
+    @Override public int getReproduction() {
+        return tempBee.getReproduction() + 2;
+    }
+    
+    /**
+     Method:  getWaxProduction
+     Inputs: 
+     Returns: 
+     
+     Description: Get the bee's ability to make wax to build the hive.
+     */
+    @Override public int getWaxProduction() {
+        return tempBee.getWaxProduction() - 3;
+    }
+    
+    /**
+     Method: getRole
+     Inputs: 
+     Returns: 
+     
+     Description: Get the bee's role such as queen, worker, drone or spawner.
+     */
+    @Override public BeeRole getRole() {
+        return BeeRole.QUEEN;
+    } 
+    
+    /**
+     * Method: toString
+     * Inputs:
+     * Outputs: String of bee attribute information
+     * 
+     * <p>Description:  Creates string containing details about the bee.
+     */
+    public String toString() {
+        
+        String beeAttributes = (getType() + " "
+                + getRole() + " " 
+                + "Gentleness: " + getGentleness() + " "
+                + "Honey Production: " + getHoneyProduction() + " "
+                + "Pollination: " + getPollination() + " "
+                + "Reproduction: " + getReproduction() + " "
+                + "Wax Production: " + getWaxProduction() + " "
+                );
+        
+        return beeAttributes;
+    }
+    
+}
